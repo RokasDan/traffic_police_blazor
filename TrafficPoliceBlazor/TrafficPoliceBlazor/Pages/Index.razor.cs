0423b886256353dfbb2d194d6d6c1ba4041f4a94
RokasDan <rokas108@gmail.com>
2023-03-23 20:31:16 +0000

    Deleted factory pages, Added new Index and Login page, added logic to index page, no css yet

---
+++ TrafficPoliceBlazor/TrafficPoliceBlazor/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TrafficPoliceBlazor.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public string headerName = "Traffic Police";
+
+        private void NavigateToPage()
+        {
+            NavigationManager.NavigateTo("/login");
+        }
+    }
+}
---
+++ TrafficPoliceBlazor/TrafficPoliceBlazor/Pages/Login.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace TrafficPoliceBlazor.Pages
+{
+    public partial class Login : ComponentBase
+    {
+
+
+    }
+}
