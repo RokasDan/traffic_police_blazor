aa2c15fa3b5526ae7c735c244ceee3eea7c13646
RokasDan <rokas108@gmail.com>
2023-03-24 18:19:21 +0000

    Found out I need a back end API for CRUD operations, clearing project for a new template

--- TrafficPoliceBlazor/TrafficPoliceBlazor/Data/officers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TrafficPoliceBlazor.Data
-{
-    public class officers
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- TrafficPoliceBlazor/TrafficPoliceBlazor/Data/unknown.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrafficPoliceBlazor.Data
-{
-    public class unknown
-    {
-        public long UnknownId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
