686abeea667a97669203238b8164d1a7599e2e4c
RokasDan <rokas108@gmail.com>
2023-03-24 19:31:05 +0000

    Added connections to mysql using adminDbcontext in program.cs

---
+++ TrafficPoliceBlazor/TrafficPoliceBlazor/Server/Dal/adminDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrafficPoliceBlazor.Shared;
+
+namespace TrafficPoliceBlazor.Server.Dal
+{
+    public class adminDbContext : DbContext
+    {
+
+        public adminDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<admins> loginDetails { get; set; }
+
+    }
+}
---
+++ TrafficPoliceBlazor/TrafficPoliceBlazor/Shared/admins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficPoliceBlazor.Shared
+{
+    public class admins
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
